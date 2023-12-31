df39a00b2d8a5fe53c8359b9ce6d4838ba4620ad
nathanclairmonte <nathanclairmonte@outlook.com>
2023-12-31 18:04:13 -0500

    add main countdown page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,141 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from "react";
+import { fireworksOptions } from "@/data/fireworksOptions";
+import { DotGothic16, Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+import Layout from "@/components/Layout";
+import { Fireworks } from "@fireworks-js/react";
+import clsx from "clsx";
+
+const gothic = DotGothic16({ weight: ["400"], subsets: ["latin"] });
+const roboto = Roboto({ weight: ["400"], subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    // const newYearDate = "2024-01-01T00:00:00";
+    const newYearDate = "2023-12-31T18:04:00";
+    const calculateTimeLeft = (targetDate) => {
+        const difference = +new Date(targetDate) - +new Date();
+        let timeLeft = {};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        if (difference > 0) {
+            timeLeft = {
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        return timeLeft;
+    };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(newYearDate));
+    const [isCelebration, setIsCelebration] = useState(false);
+    const [isClient, setIsClient] = useState(false);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+    const fireworksStyles = {
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%",
+        position: "fixed",
+    };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    // useEffect to set isClient when client mounts
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    // useEffect to update timeLeft every second
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft((prevTimeLeft) => {
+                if (
+                    prevTimeLeft.seconds > 1 ||
+                    prevTimeLeft.minutes > 0 ||
+                    prevTimeLeft.hours > 0
+                ) {
+                    const totalSeconds =
+                        prevTimeLeft.hours * 3600 +
+                        prevTimeLeft.minutes * 60 +
+                        prevTimeLeft.seconds -
+                        1;
+                    return {
+                        hours: Math.floor(totalSeconds / 3600),
+                        minutes: Math.floor((totalSeconds % 3600) / 60),
+                        seconds: totalSeconds % 60,
+                    };
+                } else {
+                    setIsCelebration(true);
+                    return prevTimeLeft;
+                }
+            });
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [timeLeft]);
+
+    const renderCountdown = () => {
+        // don't render countdown if client hasn't mounted
+        if (!isClient) return null;
+
+        // render celebration if isCelebration is true
+        if (isCelebration) {
+            return (
+                <div
+                    className={clsx(
+                        "animate-bounce text-3xl text-green-400 sm:text-5xl md:text-6xl lg:text-7xl",
+                        roboto.className
+                    )}
+                >
+                    Happy New Year!!! 🎉🎊
+                </div>
+            );
+        }
+
+        // styles for countdown rendering
+        let styleClass = "text-xl sm:text-3xl md:text-5xl text-gray-50";
+        if (timeLeft.hours === 0 && timeLeft.minutes > 0) {
+            styleClass = "text-3xl sm:text-5xl md:text-6xl text-gray-50";
+        } else if (timeLeft.hours === 0 && timeLeft.minutes === 0 && timeLeft.seconds > 0) {
+            styleClass = "text-[12rem] sm:text-[14rem] md:text-[17rem] text-gray-50";
+        }
+
+        // pluralize helper function
+        const pluralize = (count, singular) => (count === 1 ? singular : `${singular}s`);
+
+        // render countdown
+        return (
+            <div className={`countdown ${styleClass} ${gothic.className}`}>
+                {timeLeft.hours > 0 && (
+                    <span>
+                        {timeLeft.hours} {pluralize(timeLeft.hours, "hour")}{" "}
+                    </span>
+                )}
+                {timeLeft.minutes > 0 && (
+                    <span>
+                        {timeLeft.minutes} {pluralize(timeLeft.minutes, "minute")}{" "}
+                    </span>
+                )}
+                {timeLeft.seconds > 0 && (
+                    <span>
+                        {timeLeft.hours === 0 && timeLeft.minutes === 0 ? (
+                            <>{timeLeft.seconds}</>
+                        ) : (
+                            <>
+                                {timeLeft.seconds} {pluralize(timeLeft.seconds, "second")}
+                            </>
+                        )}
+                    </span>
+                )}
+            </div>
+        );
+    };
+
+    return (
+        <Layout>
+            {isCelebration && <Fireworks options={fireworksOptions} style={fireworksStyles} />}
+            <div className="absolute z-[50] flex h-full w-full flex-col items-center justify-center">
+                {renderCountdown()}
+            </div>
+        </Layout>
+    );
 }
