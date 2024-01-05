81ad8aa2f5a0dfe50bb86f932f67958176f4cb36
nathanclairmonte <nathanclairmonte@outlook.com>
2024-01-05 15:30:14 -0500

    utilise new calculate function in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { fireworksOptions } from "@/data/fireworksOptions";
 import { DotGothic16, Roboto } from "next/font/google";
-import { calculateTimeLeft } from "@/lib/utils";
+import { calculateTimeLeft, calculateTimeLeftInYear } from "@/lib/utils";
 
 import { NEW_YEAR_DATE } from "@/lib/constants";
 
@@ -19,47 +19,63 @@ export default function Home() {
     const [isCelebration, setIsCelebration] = useState(false);
     const [isClient, setIsClient] = useState(false);
 
-    const fireworksStyles = {
-        left: 0,
-        top: 0,
-        width: "100%",
-        height: "100%",
-        position: "fixed",
-    };
-
-    // useEffect to set isClient when client mounts
+    // set isClient when client mounts
+    // this is to handle hydration mismatch, workaround for now
     useEffect(() => {
         setIsClient(true);
     }, []);
 
-    // useEffect to update timeLeft every second
+    // update timeLeft every second
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setTimeLeft((prevTimeLeft) => {
-                if (
-                    prevTimeLeft.seconds > 1 ||
-                    prevTimeLeft.minutes > 0 ||
-                    prevTimeLeft.hours > 0
-                ) {
-                    const totalSeconds =
-                        prevTimeLeft.hours * 3600 +
-                        prevTimeLeft.minutes * 60 +
-                        prevTimeLeft.seconds -
-                        1;
-                    return {
-                        hours: Math.floor(totalSeconds / 3600),
-                        minutes: Math.floor((totalSeconds % 3600) / 60),
-                        seconds: totalSeconds % 60,
-                    };
-                } else {
-                    setIsCelebration(true);
-                    return prevTimeLeft;
-                }
-            });
-        }, 1000);
+        const timer = setInterval(() => {
+            const newTimeLeft = calculateTimeLeftInYear("America/New_York"); // TODO: make timezone dynamic
+            setTimeLeft(newTimeLeft);
 
-        return () => clearTimeout(timer);
-    }, [timeLeft]);
+            if (
+                newTimeLeft.months === 0 &&
+                newTimeLeft.days === 0 &&
+                newTimeLeft.hours === 0 &&
+                newTimeLeft.minutes === 0 &&
+                newTimeLeft.seconds === 0
+            ) {
+                setIsCelebration(true);
+            }
+            // else {
+            //     setIsCelebration(false);
+            // }
+        });
+
+        return () => clearInterval(timer);
+    }, []);
+
+    // // useEffect to update timeLeft every second
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         setTimeLeft((prevTimeLeft) => {
+    //             if (
+    //                 prevTimeLeft.seconds > 1 ||
+    //                 prevTimeLeft.minutes > 0 ||
+    //                 prevTimeLeft.hours > 0
+    //             ) {
+    //                 const totalSeconds =
+    //                     prevTimeLeft.hours * 3600 +
+    //                     prevTimeLeft.minutes * 60 +
+    //                     prevTimeLeft.seconds -
+    //                     1;
+    //                 return {
+    //                     hours: Math.floor(totalSeconds / 3600),
+    //                     minutes: Math.floor((totalSeconds % 3600) / 60),
+    //                     seconds: totalSeconds % 60,
+    //                 };
+    //             } else {
+    //                 setIsCelebration(true);
+    //                 return prevTimeLeft;
+    //             }
+    //         });
+    //     }, 1000);
+
+    //     return () => clearTimeout(timer);
+    // }, [timeLeft]);
 
     const renderCountdown = () => {
         // don't render countdown if client hasn't mounted
@@ -80,7 +96,16 @@ export default function Home() {
         }
 
         // styles for countdown rendering
+        // let styleClass = "text-xl sm:text-3xl md:text-5xl text-gray-50";
+        // if (timeLeft.hours === 0 && timeLeft.minutes > 0) {
+        //     styleClass = "text-3xl sm:text-5xl md:text-6xl text-gray-50";
+        // } else if (timeLeft.hours === 0 && timeLeft.minutes === 0 && timeLeft.seconds > 0) {
+        //     styleClass = "text-[12rem] sm:text-[14rem] md:text-[17rem] text-gray-50";
+        // }
         let styleClass = "text-xl sm:text-3xl md:text-5xl text-gray-50";
+        // if (timeLeft.months === 0 && timeLeft.days > 0) {
+        //     styleClass = "";
+        // }
         if (timeLeft.hours === 0 && timeLeft.minutes > 0) {
             styleClass = "text-3xl sm:text-5xl md:text-6xl text-gray-50";
         } else if (timeLeft.hours === 0 && timeLeft.minutes === 0 && timeLeft.seconds > 0) {
@@ -93,19 +118,36 @@ export default function Home() {
         // render countdown
         return (
             <div className={`countdown ${styleClass} ${gothic.className}`}>
+                {timeLeft.months > 0 && (
+                    <span>
+                        {timeLeft.months} {pluralize(timeLeft.months, "month")}
+                        {", "}
+                    </span>
+                )}
+                {timeLeft.days > 0 && (
+                    <span>
+                        {timeLeft.days} {pluralize(timeLeft.days, "day")}
+                        {", "}
+                    </span>
+                )}
                 {timeLeft.hours > 0 && (
                     <span>
-                        {timeLeft.hours} {pluralize(timeLeft.hours, "hour")}{" "}
+                        {timeLeft.hours} {pluralize(timeLeft.hours, "hour")}
+                        {", "}
                     </span>
                 )}
                 {timeLeft.minutes > 0 && (
                     <span>
-                        {timeLeft.minutes} {pluralize(timeLeft.minutes, "minute")}{" "}
+                        {timeLeft.minutes} {pluralize(timeLeft.minutes, "minute")}
+                        {", "}
                     </span>
                 )}
                 {timeLeft.seconds > 0 && (
                     <span>
-                        {timeLeft.hours === 0 && timeLeft.minutes === 0 ? (
+                        {timeLeft.months === 0 &&
+                        timeLeft.days === 0 &&
+                        timeLeft.hours === 0 &&
+                        timeLeft.minutes === 0 ? (
                             <>{timeLeft.seconds}</>
                         ) : (
                             <>
@@ -116,6 +158,14 @@ export default function Home() {
                 )}
             </div>
         );
+    };
+
+    const fireworksStyles = {
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%",
+        position: "fixed",
     };
 
     return (
