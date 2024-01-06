5dc5c21e25316fd897d37e8b17c3d16314b8aac5
nathanclairmonte <nathanclairmonte@outlook.com>
2024-01-06 11:02:22 -0500

    add calculate progress utils fn

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,7 +2,7 @@ import { DateTime } from "luxon";
 
 /**
  *
- * @param {string (IANA time zone name)} timeZone
+ * @param {string (IANA Timezone format)} timeZone
  * @returns {object} time left in year
  *
  * This function calculates the time left in the year based on the user's time zone.
@@ -36,6 +36,26 @@ export function calculateTimeLeftInYear(timeZone) {
     }
 
     return diff;
+}
+
+/**
+ *
+ * @param {string (IANA Timezone format)} timeZone
+ * @returns {number} percent progress so far in the year, granularity of days
+ *
+ * Get current DateTime, use it to get end and start of the year.
+ * Use end and start to calculate total days in year, use start and now to
+ * calculate days passed so far in the year. Then just calculate a percentage.
+ */
+export function calculatePercentProgressSoFar(timeZone) {
+    const now = DateTime.local().setZone(timeZone);
+    const startOfThisYear = DateTime.local(now.year).setZone(timeZone).startOf("year");
+    const endOfThisYear = DateTime.local(now.year).setZone(timeZone).endOf("year");
+
+    const totalDaysInYear = endOfThisYear.diff(startOfThisYear, "days").days;
+    const daysPassedSoFar = now.diff(startOfThisYear, "days").days;
+
+    return (daysPassedSoFar / totalDaysInYear) * 100;
 }
 
 /**
