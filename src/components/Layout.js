79296da57b565156a4fa0da86554cbfa7c090ad6
Nathan Clairmonte <nathanclairmonte@outlook.com>
2024-12-31 01:10:24 -0400

    add OG image generation (#19)
    * add OG image generation
    
    * update image card font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,25 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 import Footer from "@/components/Footer";
+import { calculatePercentProgressSoFar } from "@/lib/utils";
 
 const Layout = ({ children, ...customMeta }) => {
     const router = useRouter();
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        setProgress(calculatePercentProgressSoFar(userTimeZone));
+    }, []);
 
     const meta = {
         title: "NYE Countdown",
-        description: "Counting down to the New Year!",
-        image: "https://www.nyecount.com/preview.png", // remember to add our own here
+        description: `${progress.toFixed(1)}% of the way to ${
+            new Date().getFullYear() + 1
+        }!`,
+        image: `/api/og?progress=${progress}`,
         type: "website",
         ...customMeta,
     };
@@ -20,8 +30,14 @@ const Layout = ({ children, ...customMeta }) => {
                 <title>{meta.title}</title>
                 <meta name="robots" content="follow, index" />
                 <meta content={meta.description} name="description" />
-                <meta property="og:url" content={`https://www.nyecount.com${router.asPath}`} />
-                <link rel="canonical" href={`https://www.nyecount.com${router.asPath}`} />
+                <meta
+                    property="og:url"
+                    content={`https://www.nyecount.com${router.asPath}`}
+                />
+                <link
+                    rel="canonical"
+                    href={`https://www.nyecount.com${router.asPath}`}
+                />
                 <meta property="og:type" content={meta.type} />
                 <meta property="og:site_name" content="NYE Countdown" />
                 <meta property="og:description" content={meta.description} />
@@ -32,7 +48,12 @@ const Layout = ({ children, ...customMeta }) => {
                 <meta name="twitter:title" content={meta.title} />
                 <meta name="twitter:description" content={meta.description} />
                 <meta name="twitter:image" content={meta.image} />
-                {meta.date && <meta property="article:published_time" content={meta.date} />}
+                {meta.date && (
+                    <meta
+                        property="article:published_time"
+                        content={meta.date}
+                    />
+                )}
             </Head>
             <main
                 className="flex h-dvh w-full flex-col justify-between bg-black"
