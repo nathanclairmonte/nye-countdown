761733fa5a50ac58a86caa0258ed611d21e94eb1
Nathan Clairmonte <nathanclairmonte@outlook.com>
2025-12-31 20:03:39 -0400

    add umami

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,18 @@
-import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
     return (
         <>
             <Component {...pageProps} />
             <Analytics />
+            <Script
+                src="/stats/script.js"
+                data-host-url="/stats"
+                data-website-id="34013266-1edb-48aa-8927-1a6ca0f126c6"
+                strategy="afterInteractive"
+            />
         </>
     );
 }
